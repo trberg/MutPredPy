de345c63de01c73548305a933a56a2c55b931be5
Timothy Bergquist <bergqt01@HK7MQ-bergqt01.local>
2023-09-23 07:43:23 -0700

    removing duplicated scored variants

--- MutPredPy/status.py
+++ MutPredPy/status.py
@@ -51,6 +51,7 @@ class Status:
 
         if os.path.isfile(file) and os.path.getsize(file) > 0:
             scores = pd.read_csv(file)
+            scores = scores.drop_duplicates()
 
             scores = pd.DataFrame(scores.groupby("ID")["Substitution"].agg(list)).reset_index()
             scores["num_mutations"] = scores["Substitution"].apply(len).astype(int)
