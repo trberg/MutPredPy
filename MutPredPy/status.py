26ab653eebbfb15c0452fd70ca94a7bdb941bece
Timothy Bergquist <bergqt01@HK7MQ-bergqt01.local>
2023-09-29 09:32:28 -0700

    debug

--- MutPredPy/status.py
+++ MutPredPy/status.py
@@ -195,6 +195,7 @@ class Status:
         
         for index, row in logs.iterrows():
             print (row["Error"])
+            print (row["hasError"])
             if row["hasError"] and "Subscript indices must either be real positive integers or logicals." in row["Error"]:
                 output = self.get_mutpred_output_file_path(row['index'])
                 #print (output)
