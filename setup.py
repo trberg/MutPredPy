ce6c90ed706b74d6b23d4535f4eb25d8e6c028d8
Timothy Bergquist <bergqt01@HK7MQ-bergqt01.local>
2024-06-06 20:42:32 -0700

    setup

--- setup.py
+++ setup.py
@@ -10,4 +10,9 @@ with open(os.path.join(here, "MutPredPy", "__version__.py")) as f:
     exec(f.read(), about)
 # HACK: this must be kept because __init__ imports the discussion
 # modules which import requests which has to be installed first.
-setup(name="MutPredPy", version=about["__version__"], package_data={'MutPredPy':['resources/Homo_sapiens.GRCh38.combined.pep.all.fa','resources/Homo_sapiens.GRCh37.combined.pep.all.fa','resources/sequence_time.npy','resources/memory_usage.npy']})
+setup(name="MutPredPy", version=about["__version__"], package_data={'MutPredPy':[
+    'MutPredPy/resources/Homo_sapiens.GRCh38.combined.pep.all.fa',
+    'MutPredPy/resources/Homo_sapiens.GRCh37.combined.pep.all.fa',
+    'MutPredPy/resources/sequence_time.npy',
+    'MutPredPy/resources/memory_usage.npy'
+]})
