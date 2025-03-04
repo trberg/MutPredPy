260a8b20d4081399b41d9b38e6b0e6438756ea06
Timothy Bergquist <bergqt01@HK7MQ-bergqt01.local>
2025-03-04 12:59:39 -0800

    linting fixes

--- mutpredpy/configure/configure.py
+++ mutpredpy/configure/configure.py
@@ -1,20 +1,41 @@
+"""
+Configuration Module for MutPredPy
+
+This module handles loading, parsing, and managing configuration settings 
+for MutPredPy, including paths, system parameters, and runtime options.
+"""
+
+import importlib.resources as pkg_resources
 import os
 import yaml
-import importlib.resources as pkg_resources
 
 
 def load_config():
+    """
+    Loads and parses the configuration settings from a YAML file that is
+    either a user configuration file or a package specific config YAML.
+
+    Returns:
+        dict: Parsed configuration settings.
+
+    Raises:
+        FileNotFoundError: If the specified configuration file is not found.
+        yaml.YAMLError: If there is an error in parsing the YAML file.
+    """
     config = {}
 
     # 1. Load user-specific config
     user_config_path = os.path.expanduser("~/.config/mutpredpy/config.yaml")
     if os.path.exists(user_config_path):
-        with open(user_config_path, "r") as file:
+        with open(user_config_path, "r", encoding="utf-8") as file:
             config.update(yaml.safe_load(file))
 
     # 2. Load packaged default config (as fallback)
     with pkg_resources.files("mutpredpy").joinpath("config.yaml").open("r") as file:
         default_config = yaml.safe_load(file)
-        config = {**default_config, **config}  # Merge defaults with user/system settings
+        config = {
+            **default_config,
+            **config,
+        }  # Merge defaults with user/system settings
 
     return config
