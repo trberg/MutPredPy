4f8799b227e00d026f1a51b43d0d19b47a2f1a0e
Timothy Bergquist <bergqt01@HK7MQ-bergqt01.local>
2025-03-03 16:10:28 -0800

    fixing CLI

--- mutpredpy/__main__.py
+++ mutpredpy/__main__.py
@@ -25,11 +25,13 @@ from .catalog import Catalog
 
 app = typer.Typer()
 
+
 def time_minimum(x: int):
     """Ensure minimum time requirement is met."""
     if x < 2:
         raise typer.BadParameter("Minimum time is 2 hours")
     return x
+
 
 ## PREPARE
 @app.command()
@@ -39,14 +41,25 @@ def prepare(
     time: int = typer.Option(24, callback=time_minimum, help="Target time in hours"),
     dry_run: bool = typer.Option(False, help="Perform a dry run without writing files"),
     canonical: bool = typer.Option(False, help="Only prepare canonical isoforms"),
-    all_possible: bool = typer.Option(False, help="Prepare all possible amino acid substitutions"),
-    verbose: bool = typer.Option(False, help="Print additional debugging and status information during the preparation"),
-    users: int = typer.Option(1, help="Designates how many users will be running the MutPred2 scripts to better parallelize the jobs."),
-    fasta: str = typer.Option("", help="Pathway to a FASTA file from which to map the proteins/transcripts from the input file")
+    all_possible: bool = typer.Option(
+        False, help="Prepare all possible amino acid substitutions"
+    ),
+    verbose: bool = typer.Option(
+        False,
+        help="Print additional debugging and status information during the preparation",
+    ),
+    users: int = typer.Option(
+        1,
+        help="Designates how many users will be running the MutPred2 scripts to better parallelize the jobs.",
+    ),
+    fasta: str = typer.Option(
+        "",
+        help="Pathway to a FASTA file from which to map the proteins/transcripts from the input file",
+    ),
 ):
     """Prepare MutPred input files."""
     prep.Prepare(
-        input=input_file,
+        input_path=input_file,
         working_dir=working_dir,
         time=time,
         dry_run=dry_run,
@@ -54,17 +67,22 @@ def prepare(
         all_possible=all_possible,
         verbose=verbose,
         users=users,
-        fasta=fasta
+        fasta_path=fasta,
     ).prepare_mutpred_input()
+
 
 @app.command()
 def check_status(
     job_dir: str = typer.Option(..., help="Path to the MutPred2 jobs directory"),
-    summary: Optional[str] = typer.Option(None, help="Path to a pre-generated summary file"),
+    summary: Optional[str] = typer.Option(
+        None, help="Path to a pre-generated summary file"
+    ),
     show_all: bool = typer.Option(False, help="Show all job statuses"),
     show_incomplete: bool = typer.Option(False, help="Show all incomplete jobs"),
     logs: str = typer.Option("", help="Path to job logs"),
-    script: Optional[str] = typer.Option(None, help="Path to the script for error adjustment"),
+    script: Optional[str] = typer.Option(
+        None, help="Path to the script for error adjustment"
+    ),
 ):
     """Check the status of a given job."""
     status.Status(
@@ -76,12 +94,17 @@ def check_status(
         script=script,
     ).mutpred_summary()
 
+
 @app.command()
 def merge_results(
     output: str = typer.Option(..., help="Path to the output file"),
     job_dir: str = typer.Option(..., help="Path to MutPred2 jobs directory"),
-    mechanisms: bool = typer.Option(False, help="Include mechanisms and probabilities in output"),
-    dry_run: bool = typer.Option(False, help="Run merging process without saving output"),
+    mechanisms: bool = typer.Option(
+        False, help="Include mechanisms and probabilities in output"
+    ),
+    dry_run: bool = typer.Option(
+        False, help="Run merging process without saving output"
+    ),
 ):
     """Merge MutPred output files into a single file."""
     merge.Merge(
@@ -91,22 +114,25 @@ def merge_results(
         mechanisms=mechanisms,
     ).merge()
 
+
 @app.command()
 def catalog_results(
     job_dir: str = typer.Option(..., help="Path to MutPred2 jobs directory"),
-    mechanisms: bool = typer.Option(False, help="Include mechanisms in the cataloging process"),
-    dry_run: bool = typer.Option(False, help="Run merging process without saving output")
+    mechanisms: bool = typer.Option(
+        False, help="Include mechanisms in the cataloging process"
+    ),
+    dry_run: bool = typer.Option(
+        False, help="Run merging process without saving output"
+    ),
 ):
     """Catalog all mutpred scores from a given job directory"""
-    Catalog(
-        job_dir=job_dir,
-        mechanisms=mechanisms,
-        dry_run=dry_run
-    ).catalog_jobs()
+    Catalog(job_dir=job_dir, mechanisms=mechanisms, dry_run=dry_run).catalog_jobs()
+
 
 def main():
     """Run the Typer CLI application."""
     app()
+
 
 if __name__ == "__main__":
     main()
